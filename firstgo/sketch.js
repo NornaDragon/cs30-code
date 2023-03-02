d6f82dd2e2e799db986abe828a4d2c68f015935a
NornaDragon <126823407+NornaDragon@users.noreply.github.com>
2023-03-02 15:13:37 -0600

    first demo
    first day

---
+++ firstgo/sketch.js
@@ -0,0 +1,17 @@
+// Firs vs code
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  rectMode(CENTER);
+  rect(mouseX,50, 50, 100);
+}
