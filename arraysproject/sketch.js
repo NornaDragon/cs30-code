a955f72ede5caf409452ba8c24b0df579ee21179
NornaDragon <126823407+NornaDragon@users.noreply.github.com>
2023-03-08 15:00:07 -0600

    arrays and object assignment

---
+++ arraysproject/sketch.js
@@ -0,0 +1,15 @@
+// Arrays and object notion assignment (art?, genurative art?)
+// Maria van der spuy
+// 08/03/2023
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
--- index.md
+++ index.md
@@ -2,3 +2,5 @@
 
 - [firstgo](firstgo)
 - [bousing](bousing)
+- [cube](cube)
+- [array project](arraysproject)
