a4cc5721967e61ae1cab14afd737e225b7087323
NornaDragon <126823407+NornaDragon@users.noreply.github.com>
2023-03-14 15:05:26 -0600

    Update sketch.js

--- terrain/sketch.js
+++ terrain/sketch.js
@@ -18,21 +18,22 @@ function draw() {
   if (keyIsDown(RIGHT_ARROW)) {
     xOffset += 5;
   }
+
   if (keyIsDown(LEFT_ARROW)) {
     if ( xOffset > 5) {
       xOffset -= 5;
     }
     
   }
+
   for (let i = xOffset; i < xOffset + width; i++) {
     rect(terrain[i].x - xOffset, height - terrain[i].height, 1, terrain[i].height);
   }
-  
-  
 }
 
 function spawnrects() {
   let time = 0;
+  
   for (let x = 0; x < 2000; x++) {
     let h = noise(time) * height;
     let thisRect = {
@@ -42,6 +43,4 @@ function spawnrects() {
     terrain.push(thisRect);
     time += 0.01;
   }
-  
-
 }
