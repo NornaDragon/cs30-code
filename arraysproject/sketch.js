15d01c3a5bbe189a580cb4af77867abbec1b3389
NornaDragon <126823407+NornaDragon@users.noreply.github.com>
2023-03-15 14:35:45 -0600

    Update sketch.js

--- arraysproject/sketch.js
+++ arraysproject/sketch.js
@@ -9,6 +9,7 @@ let group = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
   for (let y = 0; y < height; y += 50) {
     for (let x = 0; x < width; x += 50) {
       spawnGroup(x, y, 5, 5, 1, 50, 50);
@@ -17,20 +18,63 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(185);
   displayShapes();
   damage();
+  Havelost();
+}
+
+function Havelost() {
+  for (let i = 0; i < group.length; i++){
+    if (group[i].mine && group[i].hp === 0) {
+    fill(255);
+    textSize(width / 10);
+    textAlign(CENTER, CENTER);
+    text("You Lose...", width / 2, height / 2);
+    }
+  }
 }
 
 function displayShapes() {
   for (let i = 0; i < group.length; i++) {
-    if (group[i].isMine === 1){
+
+    if (group[i].mine){
       fill(0);
       circle(group[i].x + 25, group[i].y + 25, group[i].wide);
     }
-    if (group[i].alive !== 0) {
+
+    if (group[i].displayed !== 0) {
       fill(127.5);
       rect(group[i].x, group[i].y, group[i].wide, group[i].tall);
+    }
+
+    if (!group[i].mine) {
+      textSize(group[i].wide);
+      textAlign(CENTER, CENTER);
+
+      fill(0,0,255);
+      text("1", group[i].x + 25, group[i].y + 25)
+
+      fill(0,130,0);
+      text("2", group[i].x + 25, group[i].y + 25)
+
+      fill(255,0,0);
+      text("3", group[i].x + 25, group[i].y + 25)
+
+      fill(0,0,132);
+      text("4", group[i].x + 25, group[i].y + 25)
+
+      fill(132,0,0);
+      text("5", group[i].x + 25, group[i].y + 25)
+
+      fill(0,130,132);
+      text("6", group[i].x + 25, group[i].y + 25)
+
+      fill(132,0,132);
+      text("7", group[i].x + 25, group[i].y + 25)
+
+      fill(117);
+      text("8", group[i].x + 25, group[i].y + 25)
     }
   }
 }
@@ -40,22 +84,24 @@ function damage() {
     if (group[i].hp > 0 && mouseX > group[i].x && mouseX < group[i].x + group[i].wide && mouseY > group[i].y && mouseY < group[i].y + group[i].tall && mouseIsPressed) {
       group[i].hp = group[i].hp - group[i].damage;
     }
-    else if (group[i].hp === 0 && group[i].alive !== 0) {
-      group[i].alive = group[i].alive - 1;
+    
+    else if (group[i].hp === 0 && group[i].displayed !== 0) {
+      group[i].displayed = group[i].displayed - 1;
     }
   }
 }
 
-function spawnGroup(theX, theY, theHp, theDamage, isAlive, theWidth, theHeight) {
+function spawnGroup(theX, theY, theHp, theDamage, isdisplayed, theWidth, theHeight) {
   let people = {
     x: theX,
     y: theY,
     hp: theHp,
     damage: theDamage,
-    alive: isAlive,
+    displayed: isdisplayed,
     wide: theWidth,
     tall: theHeight,
-    isMine: round(random(1)),
+    mine: random(1) > 0.9,
+    lost: false
   };
   group.push(people);
 }
