e88989ef4c32585b0809f8011860c4bb1bfd716b
NornaDragon <126823407+NornaDragon@users.noreply.github.com>
2023-03-15 16:43:13 -0600

    Update sketch.js

--- arraysproject/sketch.js
+++ arraysproject/sketch.js
@@ -19,8 +19,9 @@ function setup() {
 
 function draw() {
   background(185);
-  displayShapes();
   damage();
+  displayShapes();
+  Nums();
   Havelost();
 }
 
@@ -37,50 +38,99 @@ function Havelost() {
 
 function displayShapes() {
   for (let i = 0; i < group.length; i++) {
+    //
     if (group[i].mine){
       fill(0);
       circle(group[i].x + 25, group[i].y + 25, group[i].wide);
     }
 
+    //
     if (group[i].displayed !== 0) {
       fill(127.5);
       rect(group[i].x, group[i].y, group[i].wide, group[i].tall);
     }
-
+    
+    //
     if (group[i].hp > 0 && mouseX > group[i].x && mouseX < group[i].x + group[i].wide && mouseY > group[i].y && mouseY < group[i].y + group[i].tall && mouseIsPressed && mouseButton === RIGHT) {
       fill(255,0,0);
       triangle(group[i].x + 10, group[i].y + 10, group[i].x + 40, group[i].y + 25, group[i].x + 10, group[i].y + 40);
+    }
+  }
+}
+
+// wip
+function Nums() {
+  for (let i = 0; i < group.length; i++) {
+    for (let x = -50; x < width; x += 50) {
+      if (group[i].mine) {
+        group[i].minesSurrounding = group[i].minesSurrounding + 1;
+      }
+    }
+
+    for (let x = 50; x < width; x += 50) {
+      if (group[i].mine) {
+        group[i].minesSurrounding = group[i].minesSurrounding + 1;
+      }
+    }
+
+    for (let y = -50; y < width; y += 50) {
+      if (group[i].mine) {
+        group[i].minesSurrounding = group[i].minesSurrounding + 1;
+      }
+    }
+
+    for (let y = 50; y < width; y += 50) {
+      if (group[i].mine) {
+        group[i].minesSurrounding = group[i].minesSurrounding + 1;
+      }
     }
 
     if (!group[i].mine) {
       textSize(group[i].wide);
       textAlign(CENTER, CENTER);
 
-      fill(0,0,255);
-      text("1", group[i].x + 25, group[i].y + 25);
+      if (group[i].minesSurrounding === 1) {
+        fill(0,0,255);
+        text("1", group[i].x + 25, group[i].y + 25);
+      }
 
-      fill(0,130,0);
-      text("2", group[i].x + 25, group[i].y + 25);
+      if (group[i].minesSurrounding === 2) {
+        fill(0,130,0);
+        text("2", group[i].x + 25, group[i].y + 25);
+      }
+    
+      if (group[i].minesSurrounding === 3) {
+        fill(255,0,0);
+        text("3", group[i].x + 25, group[i].y + 25);
+      }
+    
+      if (group[i].minesSurrounding === 4) {
+        fill(0,0,132);
+        text("4", group[i].x + 25, group[i].y + 25);
+      }
 
-      fill(255,0,0);
-      text("3", group[i].x + 25, group[i].y + 25);
+      if (group[i].minesSurrounding === 5) {
+        fill(132,0,0);
+        text("5", group[i].x + 25, group[i].y + 25);
+      }
 
-      fill(0,0,132);
-      text("4", group[i].x + 25, group[i].y + 25);
+      if (group[i].minesSurrounding === 6) {
+        fill(0,130,132);
+        text("6", group[i].x + 25, group[i].y + 25);
+      }
 
-      fill(132,0,0);
-      text("5", group[i].x + 25, group[i].y + 25);
+      if (group[i].minesSurrounding === 7) {
+        fill(132,0,132);
+        text("7", group[i].x + 25, group[i].y + 25);
+      }
 
-      fill(0,130,132);
-      text("6", group[i].x + 25, group[i].y + 25);
-
-      fill(132,0,132);
-      text("7", group[i].x + 25, group[i].y + 25);
-
-      fill(117);
-      text("8", group[i].x + 25, group[i].y + 25);
+      if (group[i].minesSurrounding === 8) {
+        fill(117);
+        text("8", group[i].x + 25, group[i].y + 25);
+      }      
     }
   }
+  
 }
 
 function damage() {
@@ -104,8 +154,9 @@ function spawnGroup(theX, theY, theHp, theDamage, isdisplayed, theWidth, theHeig
     displayed: isdisplayed,
     wide: theWidth,
     tall: theHeight,
-    mine: random(1) > 0.9,
-    lost: false
+    mine: random(1) > 0.7,
+    lost: false,
+    minesSurrounding: 0,
   };
   group.push(people);
 }
