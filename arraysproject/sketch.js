1f85713d17ead8b77f5c6415908d0e8f6947b658
NornaDragon <126823407+NornaDragon@users.noreply.github.com>
2023-03-17 15:20:25 -0600

    Update sketch.js

--- arraysproject/sketch.js
+++ arraysproject/sketch.js
@@ -22,7 +22,7 @@ function draw() {
   background(185);
   damage();
   displayShapes();
-  // Nums();
+  Nums();
   Havelost();
 }
 
@@ -68,39 +68,43 @@ function Nums() {
   for (let i = 0; i < group.length; i++) {
     for (let x = 0; x < width; x += 50) {
       for (let y = 0; y < height; y += 50) {
-        if (group[i-1].mine) {
-          group[i].minesSurrounding = group[i].minesSurrounding + 1;
-        }
-
-        if (group[i+1].mine) {
-          group[i].minesSurrounding = group[i].minesSurrounding + 1;
-        }
-
-        if (group[i - group[i].y].mine) {
-          group[i].minesSurrounding = group[i].minesSurrounding + 1;
-        }
-
-        if (group[i - group[i].y - 1].mine) {
-          group[i].minesSurrounding = group[i].minesSurrounding + 1;
-        }
-
-        if (group[i - group[i].y + 1].mine) {
-          group[i].minesSurrounding = group[i].minesSurrounding + 1;
-        }
-
-        if (group[i + group[i].y].mine) {
-          group[i].minesSurrounding = group[i].minesSurrounding + 1;
-        }
-
-        if (group[i + group[i].y - 1].mine) {
-          group[i].minesSurrounding = group[i].minesSurrounding + 1;
-        }
-
-        if (group[i + group[i].y + 1].mine) {
-          group[i].minesSurrounding = group[i].minesSurrounding + 1;
-        }
+        if (!group[i].mine) {
+          if (group[i-1].mine) {
+            group[i].minesSurrounding = group[i].minesSurrounding + 1;
+          }
+  
+          if (group[i+1].mine) {
+            group[i].minesSurrounding = group[i].minesSurrounding + 1;
+          }
+  
+          if (group[i - group[i].y].mine) {
+            group[i].minesSurrounding = group[i].minesSurrounding + 1;
+          }
+  
+          if (group[i - group[i].y - 1].mine) {
+            group[i].minesSurrounding = group[i].minesSurrounding + 1;
+          }
+  
+          if (group[i - group[i].y + 1].mine) {
+            group[i].minesSurrounding = group[i].minesSurrounding + 1;
+          }
+  
+          if (group[i + group[i].y].mine) {
+            group[i].minesSurrounding = group[i].minesSurrounding + 1;
+          }
+  
+          if (group[i + group[i].y - 1].mine) {
+            group[i].minesSurrounding = group[i].minesSurrounding + 1;
+          }
+  
+          if (group[i + group[i].y + 1].mine) {
+            group[i].minesSurrounding = group[i].minesSurrounding + 1;
+          }
+        } 
       }
+      return group[i].minesSurrounding;
     }
+    
 
     
     if (!group[i].mine) {
@@ -175,7 +179,12 @@ function spawnGroup(theX, theY, theHp, theDamage, isdisplayed, theWidth, theHeig
     mine: random(1) > 0.7,
     lost: false,
     minesSurrounding: 0,
-    flag: false
+    flag: false,
+    numOfRow: 2,
+    therow: 2,
+    numOfColms: 2,
+    thecolm: 2,
+
   };
   group.push(people);
 }
