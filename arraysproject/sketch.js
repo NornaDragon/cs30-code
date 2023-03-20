fcfc1eafbddc387973e998fbe26f601708e1efe3
NornaDragon <126823407+NornaDragon@users.noreply.github.com>
2023-03-20 12:20:44 -0600

    Update sketch.js

--- arraysproject/sketch.js
+++ arraysproject/sketch.js
@@ -10,7 +10,7 @@ let group = [];
 function setup() {
   createCanvas(windowWidth, windowHeight);
   document.addEventListener("contextmenu", event => event.preventDefault());
-
+  //spawning elements
   for (let x = 0; x < width; x += 50) {
     for (let y = 0; y < height; y += 50) {
       spawnGroup(x, y, 5, 5, 1, 50, 50);
@@ -39,19 +39,19 @@ function Havelost() {
 
 function displayShapes() {
   for (let i = 0; i < group.length; i++) {
-    //
+    // displaying mines
     if (group[i].mine){
       fill(0);
       circle(group[i].x + 25, group[i].y + 25, group[i].wide);
     }
 
-    //
+    // displaying blocks
     if (group[i].displayed !== 0) {
       fill(127.5);
       rect(group[i].x, group[i].y, group[i].wide, group[i].tall);
     }
     
-    //half works flag can't go away
+    //place and removing flags with right click
     if (group[i].hp > 0 && mouseX > group[i].x && mouseX < group[i].x + group[i].wide && mouseY > group[i].y && mouseY < group[i].y + group[i].tall && mouseIsPressed && mouseButton === RIGHT) {
       group[i].flag = !group[i].flag
     }
@@ -68,93 +68,74 @@ function Nums() {
   for (let i = 0; i < group.length; i++) {
     for (let x = 0; x < width; x += 50) {
       for (let y = 0; y < height; y += 50) {
+        numOfColms = round(width/50)
+        thecolm = round(group[i].x/50 - 1)
+        numOfRow = round(height/50)
+        therow = round(group[i].y/50 - 1)
+
+        // checking if there is a mine around a non-mine square, and if there is adding a point on the minesSurrounding
+        // only brings 0
         if (!group[i].mine) {
-          if (group[int(((group[i].y * group[i].thecolm + group[i].x) - 1))].mine) {
+          if (group[i].minesSurrounding > 8 && group[int(((group[i].therow * group[i].numOfColms + group[i].thecolm) - 1))].mine === true || group[int(((group[i].therow * group[i].numOfColms + group[i].thecolm) + 1))].mine === true || group[int((group[i].therow * (group[i].numOfColms - 1) + group[i].thecolm))].mine === true || group[int((group[i].therow * (group[i].numOfColms - 1) + group[i].thecolm) - 1)].mine === true || group[int((group[i].therow * (group[i].numOfColms - 1) + group[i].thecolm) + 1)].mine === true || group[int((group[i].therow * (group[i].numOfColms + 1) + group[i].thecolm))].mine === true || group[int((group[i].therow * (group[i].numOfColms + 1) + group[i].thecolm) - 1)].mine === true || group[int((group[i].therow * (group[i].numOfColms + 1) + group[i].thecolm) + 1)].mine  === true) {
             group[i].minesSurrounding = group[i].minesSurrounding + 1;
           }
-  
-          if (group[int(((group[i].therow * group[i].thecolm + group[i].x) + 1))].mine) {
-            group[i].minesSurrounding = group[i].minesSurrounding + 1;
+        }
+        // checking minesSurrounding for a num and puting the corresponding num
+        if (!group[i].mine) {
+          textSize(group[i].wide);
+          textAlign(CENTER, CENTER);
+
+          // if (group[i].minesSurrounding === 0) {
+          //   fill(0);
+          //   text(":(", group[i].x + 25, group[i].y + 25);
+          // }
+
+          if (group[i].minesSurrounding === 1) {
+            fill(0,0,255);
+            text("1", group[i].x + 25, group[i].y + 25);
           }
-  
-          if (group[int((group[i].therow * (group[i].thecolm - 1) + group[i].x))].mine) {
-            group[i].minesSurrounding = group[i].minesSurrounding + 1;
+
+          if (group[i].minesSurrounding === 2) {
+            fill(0,130,0);
+            text("2", group[i].x + 25, group[i].y + 25);
           }
-  
-          if (group[int((group[i].therow * (group[i].thecolm - 1) + group[i].x) - 1)].mine) {
-            group[i].minesSurrounding = group[i].minesSurrounding + 1;
-          }
-  
-          if (group[int((group[i].therow * (group[i].thecolm - 1) + group[i].x) + 1)].mine) {
-            group[i].minesSurrounding = group[i].minesSurrounding + 1;
-          }
-  
-          if (group[int((group[i].therow * (group[i].thecolm + 1) + group[i].x))].mine) {
-            group[i].minesSurrounding = group[i].minesSurrounding + 1;
-          }
-  
-          if (group[int((group[i].therow * (group[i].thecolm + 1) + group[i].x) - 1)].mine) {
-            group[i].minesSurrounding = group[i].minesSurrounding + 1;
-          }
-  
-          if (int(group[(group[i].therow * (group[i].thecolm + 1) + group[i].x) + 1].mine)) {
-            group[i].minesSurrounding = group[i].minesSurrounding + 1;
-          }
-        } 
-      }
-      return group[i].minesSurrounding;
-    }
     
-
+          if (group[i].minesSurrounding === 3) {
+            fill(255,0,0);
+            text("3", group[i].x + 25, group[i].y + 25);
+          }
     
-    if (!group[i].mine) {
-      textSize(group[i].wide);
-      textAlign(CENTER, CENTER);
+          if (group[i].minesSurrounding === 4) {
+            fill(0,0,132);
+            text("4", group[i].x + 25, group[i].y + 25);
+          }
 
-      if (group[i].minesSurrounding === 1) {
-        fill(0,0,255);
-        text("1", group[i].x + 25, group[i].y + 25);
-      }
+          if (group[i].minesSurrounding === 5) {
+            fill(132,0,0);
+            text("5", group[i].x + 25, group[i].y + 25);
+          }
 
-      if (group[i].minesSurrounding === 2) {
-        fill(0,130,0);
-        text("2", group[i].x + 25, group[i].y + 25);
-      }
-    
-      if (group[i].minesSurrounding === 3) {
-        fill(255,0,0);
-        text("3", group[i].x + 25, group[i].y + 25);
-      }
-    
-      if (group[i].minesSurrounding === 4) {
-        fill(0,0,132);
-        text("4", group[i].x + 25, group[i].y + 25);
-      }
+          if (group[i].minesSurrounding === 6) {
+            fill(0,130,132);
+            text("6", group[i].x + 25, group[i].y + 25);
+          }
 
-      if (group[i].minesSurrounding === 5) {
-        fill(132,0,0);
-        text("5", group[i].x + 25, group[i].y + 25);
-      }
+          if (group[i].minesSurrounding === 7) {
+            fill(132,0,132);
+            text("7", group[i].x + 25, group[i].y + 25);
+          }
 
-      if (group[i].minesSurrounding === 6) {
-        fill(0,130,132);
-        text("6", group[i].x + 25, group[i].y + 25);
+          if (group[i].minesSurrounding === 8) {
+            fill(117);
+            text("8", group[i].x + 25, group[i].y + 25);
+          }      
+        }
       }
-
-      if (group[i].minesSurrounding === 7) {
-        fill(132,0,132);
-        text("7", group[i].x + 25, group[i].y + 25);
-      }
-
-      if (group[i].minesSurrounding === 8) {
-        fill(117);
-        text("8", group[i].x + 25, group[i].y + 25);
-      }      
     }
   }
-  return group[0].minesSurrounding;
 }
 
+// put blocks on top of everything until they are clicked
 function damage() {
   for (let i = 0; i < group.length; i++) {
     if (group[i].flag === false && group[i].hp > 0 && mouseX > group[i].x && mouseX < group[i].x + group[i].wide && mouseY > group[i].y && mouseY < group[i].y + group[i].tall && mouseIsPressed && mouseButton === LEFT) {
@@ -167,7 +148,7 @@ function damage() {
   }
 }
 
-function spawnGroup(theX, theY, theHp, theDamage, isdisplayed, theWidth, theHeight) {
+function spawnGroup(theX, theY, theHp, theDamage, isdisplayed, theWidth, theHeight, allColm, whatColm, allRow, whatRow) {
   let people = {
     x: theX,
     y: theY,
@@ -180,13 +161,10 @@ function spawnGroup(theX, theY, theHp, theDamage, isdisplayed, theWidth, theHeig
     lost: false,
     minesSurrounding: 0,
     flag: false,
-
-
-    //rework!
-    numOfColms: round(width/50 + 1),
-    thecolm: round(theX/50),
-    numOfRow: round(height/50 + 1),
-    therow: round(theY/50),
+    numOfColms: allColm,
+    thecolm: whatColm,
+    numOfRow: allRow,
+    therow: whatRow,
   };
   group.push(people);
 }
