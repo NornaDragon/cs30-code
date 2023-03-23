bf431b796f9da094f2c8492ef9279420f389c04b
NornaDragon <126823407+NornaDragon@users.noreply.github.com>
2023-03-23 15:29:11 -0600

    Update sketch.js

--- 2d-array-project/sketch.js
+++ 2d-array-project/sketch.js
@@ -5,15 +5,50 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-const ROWS = 15;
+const ROWS = 5;
 const COLS = 15;
 let grid;
-let celsize;
+let cellSize;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  grid = createEmptyDungeon(ROWS, COLS);
+  
+  
+  if(width < height) {
+    cellSize = width/COLS;
+  }
+  else {
+    cellSize = height/ROWS;
+  }
 }
 
 function draw() {
   background(220);
+  displayDungeon(grid);
+}
+
+function displayDungeon(grid) {
+  for (let y = 0; y < ROWS; y++) {
+    for (let x = 0; x < COLS; x++) {
+      if (grid[y][x] === 0) {
+        fill("brown");
+      }
+      if (grid[y][x] === 1) {
+        fill("black");
+      }
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+function createEmptyDungeon(ROWS, COLS) {
+  let newGrid = [];
+  for (let y = 0; y < ROWS; y++) {
+    newGrid.push([]);
+    for (let x = 0; x < COLS; x++) {
+      newGrid[y].push(0);
+    }
+  }
+  return newGrid;
 }
