cca9b34aa33a8a8b2e38f769647eb73e60d9121c
NornaDragon <126823407+NornaDragon@users.noreply.github.com>
2023-04-17 15:19:23 -0600

    Update sketch.js

--- walker-OOP/sketch.js
+++ walker-OOP/sketch.js
@@ -1,15 +1,50 @@
 // Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
 
+class Walker {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.colour = "red";
+    this.speed = 5;
+    this.size = 5;
+  }
+
+  display() {
+    noStroke();
+    fill(this.colour);
+    circle(this.x, this.y, this.size);
+  }
+  
+  move() {
+    let choice = random(100);
+    if (choice < 25) {
+      this.y -= this.speed;
+    }
+    else if (choice < 50) {
+      this.y += this.speed;
+    }
+    else if (choice < 75) {
+      this.x -= this.speed;
+    }
+    else if (choice < 100) {
+      this.x += this.speed;
+    }
+  }
+}
+
+let kevin;
+let wasi;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  kevin = new Walker(width/2, height/2);
+  wasi = new Walker(200, 400);
+  wasi.colour = "blue";
 }
 
 function draw() {
-  background(220);
+  kevin.move();
+  kevin.display();
+  wasi.move();
+  wasi.display();
 }
